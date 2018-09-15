443566da14afd14439b090b4e027e692fc056631
Yicun_Hou <yicunhou@bu.edu>
2018-09-15 16:49:30 -0400

    Adjust the script structure

--- api_project.py
+++ api_project.py
@@ -5,29 +5,47 @@
 import tweepy
 import wget
 import subprocess
+import os
 
 consumer_key = "3NaLT71Ac76C9JH6tf5YYKXJr"
 consumer_secret = "iHQeyLQMQBdROGTzWDtmPZAcIGbdAKCG44TnaJgFVwW8HXlZLr"
 access_token = "3895743257-Ai6TaeldDTIu22YA4w6m9lFne54aejVwoIdBqDh"
 access_token_secret = "5GOWjN7i2bOVKP1k56pC5NdBYnNkQcdlG231fOWcNzrNA"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def download_img():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(id = "IKEAUSA", page = 10)
+    image_link = []
 
-api = tweepy.API(auth)
-tweets = api.user_timeline(id = "IKEAUSA", page = 10)
-image_link = []
+    for tweet in tweets:
+        media = tweet.entities.get('media', [])
+        if(len(media) > 0):
+            image_link.append(media[0]['media_url'])
 
-for tweet in tweets:
-    media = tweet.entities.get('media', [])
-    if(len(media) > 0):
-        image_link.append(media[0]['media_url'])
+    i = 1
+    for image in image_link:
+        print("Downloading image %s" %i)
+        wget.download(image)
+        print('\n')
+        i += 1
 
-i = 1
-for image in image_link:
-    print("Downloading image %s" %i)
-    wget.download(image)
-    print('\n')
-    i += 1
+def rename_img(imgpath):
+    img_list = os.listdir(imgpath)
+    i = 0
+    
+    for img in img_list:
+        if img.endswith('.jpg'):
+            src = os.path.join(os.path.abspath(imgpath), img)
+            dst = os.path.join(os.path.abspath(imgpath), format('img' + str(i), '0>3s') + '.jpg')
+            os.rename(src, dst)
+            i = i + 1
 
-print(image_link)
+def main():
+    download_img()
+    path = os.getcwd()
+    rename_img(path)
+
+if __name__ == '__main__':
+    main()
